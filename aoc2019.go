ebf1efb2c15638cb81527f9340548f5cc90d6c32
Geoff Sullivan <geoffssullivan@gmail.com>
2019-12-02 01:55:16 -0500

    Removes individual day functions

--- aoc2019.go
+++ aoc2019.go
@@ -9,12 +9,13 @@ import (
 
 type day struct {
 	key int
-	fun func()
+	ans1 func() string
+	ans2 func() string
 }
 
 var days = []day{
-	{1, d1},
-	{2, d2},
+	{1, day01.Answer1, day01.Answer2},
+	{2, day02.Answer1, day02.Answer2},
 }
 
 func main() {
@@ -26,25 +27,20 @@ func main() {
 	for _, d = range days {
 		for _, i := range selected {
 			if d.key == i {
-				fmt.Printf("Day %d\n", d.key)
-				d.fun()
+				run(d)
 				found = true
 			}
 		}
 	}
 
+	// none selected, run latest
 	if !found {
-		fmt.Printf("Day %d\n", d.key)
-		d.fun()
+		run(d)
 	}
 }
 
-func d2() {
-	fmt.Printf("Puzzle 1 Answer: %s\n", day02.Answer1())
-	fmt.Printf("Puzzle 2 Answer: %s\n", day02.Answer2())
-}
-
-func d1() {
-	fmt.Printf("Puzzle 1 Answer: %s\n", day01.Answer1())
-	fmt.Printf("Puzzle 2 Answer: %s\n", day01.Answer2())
+func run(d day) {
+	fmt.Printf("------- Day %d -------\n", d.key)
+	fmt.Printf("Puzzle 1 Answer: %s\n", d.ans1())
+	fmt.Printf("Puzzle 2 Answer: %s\n", d.ans2())
 }
